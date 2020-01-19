f212caff0c9282bcf83088b28e07894dd66ce4e1
Dionte <Dionte_sb@yahoo.com>
2020-01-18 20:53:25 -0800

    Added the sign up view controller

---
+++ LoginScreenTest/Controllers/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  LoginScreenTest
+//
+//  Created by Dionte Silmon on 1/18/20.
+//  Copyright © 2020 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
